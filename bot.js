3cecc2014b4b4fda84fe27c706c51eb4eab9b0bf
blackopss <36933564+blackopss@users.noreply.github.com>
2018-08-08 02:20:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,9 +17,30 @@ let playlist = [];
 
 client.login(config.secret.token);
 
-client.once('ready', () => {
-    console.log(chalk.greenBright.underline("\nBot online.\n"));
-    client.user.setGame(null);
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`security`,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
 
 client.on('message', (message) => {
